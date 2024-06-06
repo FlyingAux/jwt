b3d50ffdaabfe7c360845ddcfbdbce767675d0b1
rajdev04 <rajdevshrivastava72@gmail.com>
2024-06-06 19:09:13 +0530

    jwt

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-const cookieParser = require('cookie-parser');
-var express = require('express');
+const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
 
-app.get('/',function(req,res){
-    res.cookie("name", "raj")
-    res.send('done')
+
+app.get('/',function(req,res,next){
+    var token = jwt.sign({ email: 'raj@example.com' }, 'shhhhh');
+    // console.log(res.cookie('token', token))
+    res.cookie('token', token);
+    res.send('Done')
 })
 
-app.get('/read',function(req,res){
-    console.log(req.cookies);
-    res.send('done hogaya')
+app.get('/read',function(req,res,next){
+    let data = jwt.verify(req.cookies.token,"shhhhh");
+    console.log(data);
+    res.send('reading');
 })
-
 
 app.listen(3000);
